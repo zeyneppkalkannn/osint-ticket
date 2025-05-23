ff2a37c87879fdcf7347acfaacd252aebd85aec1
zeyneppkalkannn <140957398+zeyneppkalkannn@users.noreply.github.com>
2025-05-23 13:49:07 +0300

    script

---
+++ script.js
@@ -0,0 +1,23 @@
+function checkForm() {
+    const pnr = document.getElementById("pnr").value.trim();
+    const captcha = document.getElementById("captcha").value.trim();
+  
+    if (pnr === "GN8721" && captcha === "9") {
+      document.getElementById("result").innerHTML = `
+        <h3>BOARDING PASS</h3>
+        <p>Name: Ülfet Yaylacı</p>
+        <p>Flight: TK1974</p>
+        <p>From: Ankara Esenboğa Airport</p>
+        <p>To: 33.3149413°, 35.3237104°</p>
+        <p>Date: 2025-05-31</p>
+        <p>Time: 01:30</p>
+        <p>Seat: 18A</p>
+        <p>Gate: B7</p>
+        <p>Boarding Time: 01.00</p>
+      `;
+    } else {
+      alert("Hatalı PNR veya CAPTCHA.");
+    }
+  
+    return false; // Sayfa yenilenmesin
+  }
